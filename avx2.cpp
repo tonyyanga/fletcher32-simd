1b0ec24dfca9d8817105f2dd55bbaa3d846fdd96
Tony Yang <tonyyanga@gmail.com>
2017-10-03 22:24:48 -0700

    attempt to use longer batch 359 * 8, incorrect result on b

--- avx2.cpp
+++ avx2.cpp
@@ -17,7 +17,7 @@ uint32_t fletcher32_avx2 (uint16_t* data, size_t len, uint32_t& a, uint32_t& b) 
     uint64_t temp_b = b; // prevent overflow in b
 
     while (len >= 8) {
-        tlen = (len >= 359) ? 359 : len;
+        tlen = (len >= 359 * 8) ? 359 * 8 : len;
         tlen = tlen - tlen % 8;
         len -= tlen;
 
